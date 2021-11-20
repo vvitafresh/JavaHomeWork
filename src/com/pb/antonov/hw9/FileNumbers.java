afe7385beaa63e822797dd7f9c4f0c36d1134c9e
vvitafresh <he250679avv@gmail.com>
2021-11-20 19:50:05 +0200

    hw9 commit 02

---
+++ src/com/pb/antonov/hw9/FileNumbers.java
@@ -0,0 +1,17 @@
+package com.pb.antonov.hw9;
+
+public class FileNumbers {
+    public static void main(String[] args) {
+        final String fileName="numbers.txt";
+        createNumbersFile(fileName);
+        createOddNumbersFile(fileName);
+    }
+
+    public static void createNumbersFile(String fileName){
+
+    }
+
+    public static void createOddNumbersFile(String fileName){
+
+    }
+}
