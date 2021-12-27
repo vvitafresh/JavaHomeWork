1364eb4922469a76fc3bd4aa6b68d05aab591a1d
vvitafresh <he250679avv@gmail.com>
2021-12-27 22:27:35 +0200

    hw13 commit 01

---
+++ src/com/pb/antonov/hw13/Main.java
@@ -0,0 +1,8 @@
+package com.pb.antonov.hw13;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Placeholder for homework!");
+        System.out.println("I didn't have time to finish Homework due to vacations ((");
+    }
+}
